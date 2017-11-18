0e488860fe575f474dd54bcd703871e4bc354462
evaparicio Evelyn <ev.aparicio.c@gmail.com>
2017-11-18 03:24:06 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -20,6 +20,8 @@ class Question {
 }
 Question._All =[]
 
-const question = new Question("What's your favorite food?")
-question.save()
+const question1 = new Question("What's your favorite food?")
+const question2 = new Question("What city were you born in?")
+const question3 = new Question("What's your favorite TV Show")
+
 Question.All()
